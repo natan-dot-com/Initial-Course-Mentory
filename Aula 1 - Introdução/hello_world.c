fbf1fb2a484c5a43deedf96b933d165d2ad8142e
Natan <natanhsanches@gmail.com>
2021-03-08 15:43:38 -0300

    First class: Introduction

---
+++ Aula 1 - Introdução/hello_world.c
@@ -0,0 +1,15 @@
+// Incluindo o cabeçalho da biblioteca Standard Input-Output
+#include <stdio.h>
+
+// A função main é a função principal do código. Nela que parte a execução
+// do código e ela pode conter outras funções.
+int main() {
+
+    // Invocando a função printf, da biblioteca stdio.h (incluida na primeira linha).
+    //  A função print exibe na tela o que tem no interior dela.
+    printf("Hello World!\n");
+
+    // Como a função main pode ser executada por completo, deve-se retornar 0 ao
+    // sistema operacional para informar que a ação foi bom executada.
+    return 0;
+}
